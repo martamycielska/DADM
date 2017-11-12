e5718ce2662c4efcd6565814327af77fb5d85258
Michal <michalt.krakow@gmail.com>
2017-11-12 22:57:40 +0100

    update

--- config/pngReader/mainwindow.h
+++ config/pngReader/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "vtkImageViewer.h"
+#include "vtkSmartPointer.h"
 
 namespace Ui {
 class MainWindow;
@@ -17,6 +19,7 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    vtkSmartPointer<vtkImageViewer> image_view;
 };
 
 #endif // MAINWINDOW_H
