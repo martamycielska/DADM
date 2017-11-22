69bf730edb800917be6308ac8c5b2622a76d1555
aneti9862 <aneti9862@gmail.com>
2017-11-22 20:22:35 +0100

    wszystko dziala poprawnie

--- DADM/visualization3d.cpp
+++ DADM/visualization3d.cpp
@@ -27,7 +27,7 @@ Visualization3D::Visualization3D(QWidget *parent) :
 
 void Visualization3D::brain3D(){
     ui->processDescLabel->setText("The model 3D initializing ...");
-    const char* fileName = "/home/aneta/Desktop/DADMgit/DADM/DADM/8bit.raw";
+    const char* fileName = "/home/aneta/Pulpit/DADMgit/DADM/DADM/8bit.raw";
     float threshold = 100;
     int extractLargest = 0;
 
