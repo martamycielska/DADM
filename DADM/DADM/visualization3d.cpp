de9563c54d1d362972a98ca1e3595663a26201a6
Aneta <aneti9862@gmail.com>
2017-12-10 14:37:15 +0100

    Brain 3D - update

--- DADM/DADM/visualization3d.cpp
+++ DADM/DADM/visualization3d.cpp
@@ -21,13 +21,13 @@ Visualization3D::Visualization3D(QWidget *parent) :
 #pragma region controls event
 
 void Visualization3D::Brain3D() {
-	UpdateProcessStateText("The model 3D initializing ....\nPlease wait");
 	QUrl url = QFileDialog::getOpenFileUrl(this, "Open file", QUrl(""), "Raw file (*.raw) (*raw)");
 
 	if (url.isEmpty())
 		return;
 
 	QString path = url.path().remove(0, 1);
+	UpdateProcessStateText("The model 3D initializing ....\nPlease wait");
 	brain_3D = new Brain_3D(path, xspace, yspace, zspace, threshold, shrinkingFactor);
 
 	worker = new VisualizationWorker(brain_3D);
@@ -54,43 +54,46 @@ void Visualization3D::ShrinkSliderValueChanged(int sliderValue) {
 	}
 }
 
-void Visualization3D::CutEnableChanged(bool cutEnable) {
-	if (visualizationDone) {
-		if (cutEnable)
-			planeWidget -> On();
-		else
-			planeWidget -> Off();
-
-		ui -> qvtkWidget -> GetRenderWindow() -> Render();
-	}
-	else {
-		if (cutEnable)
-			setCutOptionEnable = true;
-		else
-			setCutOptionEnable = false;
-	}
-}
+//void Visualization3D::CutEnableChanged(bool cutEnable) {
+//	if (visualizationDone) {
+//		if (cutEnable)
+//			planeWidget -> On();
+//		else
+//			planeWidget -> Off();
+//
+//		ui -> qvtkWidget -> GetRenderWindow() -> Render();
+//	}
+//	else {
+//		if (cutEnable)
+//			setCutOptionEnable = true;
+//		else
+//			setCutOptionEnable = false;
+//	}
+//}
 
 void Visualization3D::AcceptThreshold() {
-	UpdateProcessStateText("The model 3D rendering...\nPlease wait");
-
-	QEventLoop loop;
-	QTimer::singleShot(1000, &loop, SLOT(quit()));
-	loop.exec();
-
 	QString thresholdString = ui -> thresholdTextEdit -> toPlainText();
 	float thresholdFloat = thresholdString.toFloat();
 	threshold = thresholdFloat;
 
 	if (visualizationDone == true) {
+		UpdateProcessStateText("The model 3D rendering...\nPlease wait");
+
+		QEventLoop loop;
+		QTimer::singleShot(1000, &loop, SLOT(quit()));
+		loop.exec();
+
 		MarchingCubes mc = brain_3D -> getMarchingCubes();
 		mc -> SetValue(0, thresholdFloat);
 		brain_3D -> setMarchingCubes(mc);
 		brain_3D -> setThreshold(threshold);
 		ui->qvtkWidget -> GetRenderWindow() -> Render();
-	}
 
-	UpdateProcessStateText("Rendering done");
+		UpdateProcessStateText("Rendering done");
+	}
+	else {
+		UpdateProcessStateText("Threshold changing done");
+	}
 }
 #pragma endregion controls event
 
@@ -103,14 +106,14 @@ void Visualization3D::InitValue() {
 	threshold = 84;
 	shrinkingFactor = 2;
 	visualizationDone = false;
-	setCutOptionEnable = true;
+	//setCutOptionEnable = true;
 }
 
 void Visualization3D::SetConnections() {
 	connect(ui->visualizeBtn, SIGNAL(clicked(bool)), this, SLOT(Brain3D()));
 	connect(ui->horizontalSlider, SIGNAL(valueChanged(int)), this, SLOT(SliderValueChanged(int)));
 	connect(ui->acceptBtn, SIGNAL(clicked(bool)), this, SLOT(AcceptThreshold()));
-	connect(ui->cutEnableRadioBtn, SIGNAL(toggled(bool)), this, SLOT(CutEnableChanged(bool)));
+	//connect(ui->cutEnableRadioBtn, SIGNAL(toggled(bool)), this, SLOT(CutEnableChanged(bool)));
 	connect(ui->shrinkSlider, SIGNAL(valueChanged(int)), this, SLOT(ShrinkSliderValueChanged(int)));
 }
 
@@ -125,50 +128,50 @@ void Visualization3D::AddRendererAndPlaneWidget() {
 	ui->qvtkWidget->SetRenderWindow(renderWnd);
 	ui->qvtkWidget->GetRenderWindow()->AddRenderer(brain_3D->getData());
 
-	plane = vtkSmartPointer<vtkPlane>::New();
-	plane->SetNormal(1, 0, 0);
-	clipper = vtkSmartPointer<vtkClipPolyData>::New();
-	clipper->SetClipFunction(plane);
-	clipper->InsideOutOn();
+	//plane = vtkSmartPointer<vtkPlane>::New();
+	//plane->SetNormal(1, 0, 0);
+	//clipper = vtkSmartPointer<vtkClipPolyData>::New();
+	//clipper->SetClipFunction(plane);
+	//clipper->InsideOutOn();
 
-	myCallback = vtkSmartPointer<MyCallback>::New();
-	myCallback->Plane = plane;
+	//myCallback = vtkSmartPointer<MyCallback>::New();
+	//myCallback->Plane = plane;
 
-	rep = vtkSmartPointer<vtkImplicitPlaneRepresentation>::New();
-	rep->SetPlaceFactor(1.0); // This must be set prior to placing the widget
-	rep->SetNormal(plane->GetNormal());
-	rep->OutlineTranslationOff();
-	rep->ScaleEnabledOff();
-	rep->GetPlaneProperty()->SetOpacity(0.0);
-	rep->GetSelectedPlaneProperty()->SetOpacity(0.2);
+	//rep = vtkSmartPointer<vtkImplicitPlaneRepresentation>::New();
+	//rep->SetPlaceFactor(1.0); // This must be set prior to placing the widget
+	//rep->SetNormal(plane->GetNormal());
+	//rep->OutlineTranslationOff();
+	//rep->ScaleEnabledOff();
+	//rep->GetPlaneProperty()->SetOpacity(0.0);
+	//rep->GetSelectedPlaneProperty()->SetOpacity(0.2);
 
-	planeWidget = vtkSmartPointer<vtkImplicitPlaneWidget2>::New();
-	planeWidget->SetInteractor(ui->qvtkWidget->GetInteractor());
-	planeWidget->SetRepresentation(rep);
-	planeWidget->AddObserver(vtkCommand::InteractionEvent, myCallback);
+	//planeWidget = vtkSmartPointer<vtkImplicitPlaneWidget2>::New();
+	//planeWidget->SetInteractor(ui->qvtkWidget->GetInteractor());
+	//planeWidget->SetRepresentation(rep);
+	//planeWidget->AddObserver(vtkCommand::InteractionEvent, myCallback);
 
-	if (setCutOptionEnable)
-		planeWidget->On();
+	//if (setCutOptionEnable)
+	//	planeWidget->On();
 
-	clipper->SetInputConnection(brain_3D->getConfilter()->GetOutputPort());
-	brain_3D->getMapper()->SetInputConnection(clipper->GetOutputPort());
+	//clipper->SetInputConnection(brain_3D->getConfilter()->GetOutputPort());
+	//brain_3D->getMapper()->SetInputConnection(clipper->GetOutputPort());
 
-	plane->SetOrigin(xspace / 2., yspace / 2., zspace / 2.);
-	plane->SetNormal(0, -1, 0);
-	rep->SetNormal(plane->GetNormal());
+	//plane->SetOrigin(xspace / 2., yspace / 2., zspace / 2.);
+	//plane->SetNormal(0, -1, 0);
+	//rep->SetNormal(plane->GetNormal());
 
-	double bounds[6];
-	bounds[0] = 0;
-	bounds[1] = xspace;
-	bounds[2] = 0;
-	bounds[3] = yspace;
-	bounds[4] = 0;
-	bounds[5] = zspace;
+	//double bounds[6];
+	//bounds[0] = 0;
+	//bounds[1] = xspace;
+	//bounds[2] = 0;
+	//bounds[3] = yspace;
+	//bounds[4] = 0;
+	//bounds[5] = zspace;
 
-	plane->SetNormal(1., 0., 0.);
-	rep->PlaceWidget(bounds);
-	rep->SetOrigin(plane->GetOrigin());
-	rep->SetNormal(plane->GetNormal());
+	//plane->SetNormal(1., 0., 0.);
+	//rep->PlaceWidget(bounds);
+	//rep->SetOrigin(plane->GetOrigin());
+	//rep->SetNormal(plane->GetNormal());
 
 	ui->qvtkWidget->repaint();
 	visualizationDone = true;
