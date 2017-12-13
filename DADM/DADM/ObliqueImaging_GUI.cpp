27db85699fd5e8c4aa1781c02cf1e4eec1720937
szymaa <szymon.w77@gmail.com>
2017-12-13 19:18:17 +0100

    Add pre-alpha UI

---
+++ DADM/DADM/ObliqueImaging_GUI.cpp
@@ -0,0 +1,12 @@
+#include "ObliqueImaging_GUI.h"
+
+ObliqueImaging_GUI::ObliqueImaging_GUI(QWidget *parent)
+	: QWidget(parent)
+{
+	ui.setupUi(this);
+}
+
+ObliqueImaging_GUI::~ObliqueImaging_GUI()
+{
+
+}
