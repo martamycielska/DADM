649ed4b5b37b029290721a8b8a3040c7d5dac811
magdahetmanska <hetmanska.magda@gmail.com>
2017-12-17 11:26:13 +0100

    MRI reconstruction

--- DADM/DADM/Reconstruction.cpp
+++ DADM/DADM/Reconstruction.cpp
@@ -18,8 +18,35 @@ Reconstruction::Reconstruction(Data4DRaw data)
 
 void Reconstruction::StructuralDataAlgorithm() {
 
+
 }
 
 void Reconstruction::DiffusionDataAlgorithm() {
+
+}
+
+void Reconstruction::FourierTransform() {
+	
+	Data3DRaw raw_data = data3DRaw_input;
+	Eigen::FFT_REVERSE;
+	size_t dim_x = 28, dim_y = 126;
+	Eigen::FFT<float> fft;
+	Eigen::MatrixXf in = Eigen::MatrixXf::Random(dim_x, dim_y);
+	Eigen::MatrixXcf out;
+	out.setZero(dim_x, dim_y);
+
+	for (int k = 0; k < in.rows(); k++) {
+		Eigen::VectorXcf tmpOut(dim_x);
+		fft.fwd(tmpOut, in.row(k));
+		out.row(k) = tmpOut;
+	}
+
+	for (int k = 0; k < in.cols(); k++) {
+		Eigen::VectorXcf tmpOut(dim_y);
+		fft.fwd(tmpOut, out.col(k));
+		out.col(k) = tmpOut;
+	}
+
+
 
 }
--- DADM/DADM/Reconstruction.h
+++ DADM/DADM/Reconstruction.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Diffusion_Structural_Module.h"
 #include <Eigen\Eigenvalues>
+#include <unsupported/Eigen/FFT>
 
 //typedef Tensor<std::complex<double>, 3> Data3DRaw;
 //typedef Tensor<std::complex<double>, 4> Data4DRaw;
@@ -19,6 +20,7 @@ public:
 private:
 	virtual void StructuralDataAlgorithm();
 	virtual void DiffusionDataAlgorithm();
+	void FourierTransform();
 	Data3DRaw data3DRaw_input;
 	Data4DRaw data4DRaw_input;
 };
