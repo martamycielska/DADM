7c7f01d671517da7fb86622337e01695b1afb519
unknown <michalt.krakow@gmail.com>
2017-12-29 15:04:14 +0100

    Poprawa Globlas i Import File

--- x64/DADM/DADM/Globals.cpp
+++ x64/DADM/DADM/Globals.cpp
@@ -1,1 +1,17 @@
 #include "Globals.h"
+
+namespace Global {
+	
+	extern Data3D structuralData = Data3D(0);
+	extern Data3DRaw structuralRawData = Data3DRaw(0);
+	extern Data3DRaw structuralSensitivityMaps = Data3DRaw(0);
+	extern double L = 0;
+	extern double r = 0;
+	extern Data4D diffusionData4D = Data4D(0);
+	extern Data3D diffusionData3D = Data3D(0);
+	extern SegmentationData segmentationData;
+	extern MatrixXd FA;
+	extern MatrixXd MD;
+	extern MatrixXd RA;
+	extern MatrixXd VR;
+}
--- x64/DADM/DADM/Globals.h
+++ x64/DADM/DADM/Globals.h
@@ -5,16 +5,17 @@
 #include "Segmentation.h"
 #include "Reconstruction.h"
 
-extern Data3D structuralData;
-extern Data3DRaw structuralRawData;
-extern Data3DRaw structuralSensitivityMaps;
-extern int L;
-extern int r;
-extern Data4D diffusionData4D;
-extern Data3D diffusionData3D;
-extern SegmentationData segmentationData;
-extern MatrixXd FA;
-extern MatrixXd MD;
-extern MatrixXd RA;
-extern MatrixXd VR;
-
+namespace Global {
+	extern Data3D structuralData;
+	extern Data3DRaw structuralRawData;
+	extern Data3DRaw structuralSensitivityMaps;
+	extern double L;
+	extern double r;
+	extern Data4D diffusionData4D;
+	extern Data3D diffusionData3D;
+	extern SegmentationData segmentationData;
+	extern MatrixXd FA;
+	extern MatrixXd MD;
+	extern MatrixXd RA;
+	extern MatrixXd VR;
+}
