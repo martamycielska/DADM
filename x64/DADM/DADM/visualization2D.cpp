25a0489adb07059a9e52561e71323dec56953f8b
magdakopciuch <33605249+magdakopciuch@users.noreply.github.com>
2018-01-10 20:09:29 +0100

    klasa wizualizacji 2D

---
+++ x64/DADM/DADM/visualization2D.cpp
@@ -0,0 +1,113 @@
+#include "visualization2D.h"
+#include "ui_visualization2D.h"
+#include <QThread>
+#include <QDebug>
+#include <QUrl>
+#include <QString>
+#include <QFileDialog>
+#include <QTimer>
+#include "Globals.h"
+#include "helpermethods.h"
+#include "qdebug.h"
+#include "dadm.h"
+#include <Eigen\Dense>
+#include <iostream>
+#include <vtkStructuredPointsReader.h>
+#include <vtkRendererCollection.h>
+#include <vtkSmartPointer.h>
+#include <vtkImageImport.h>
+#include <vtkImageActor.h>
+#include <stdlib.h>
+
+
+
+Visualization2D::Visualization2D(QWidget *parent) :
+	QMainWindow(parent),
+	ui(new Ui::Visualization2D)
+{
+	InitValue();
+	SetConnections();
+	InitUI();
+}
+
+void Visualization2D::Brain2D() {
+	
+	QUrl url = QFileDialog::getOpenFileUrl(this, "Open file", QUrl(""), "Mat file (*.mat) (*mat)");
+
+	if (url.isEmpty())
+		return;
+
+	QString path = url.path().remove(0, 1);
+	brain_2D = new Brain_2D(path);
+
+
+	//c-style image test image
+	const int width = 4;
+	const int height = 4;
+	const int depth = 4;
+
+	unsigned char cImage[width * height * depth];
+	unsigned char value = 0;
+	for (unsigned int row = 0; row < height; ++row) {
+		for (unsigned int col = 0; col < width; ++col) {
+			for (unsigned int z = 0; z < depth; ++z) {
+				cImage[row * width * depth + col * depth + z] = 100;
+			}
+		}
+	}
+
+	//tu dodaæ definicjê wskaŸnika cImage do macierzy danych
+
+	vtkSmartPointer<vtkImageImport> imageImport =
+		vtkSmartPointer<vtkImageImport>::New();
+	imageImport->SetDataSpacing(1, 1, 1);
+	imageImport->SetDataOrigin(0, 0, 0);
+	imageImport->SetWholeExtent(0, width-1, 0, height-1, 0, 0);
+	imageImport->SetDataExtentToWholeExtent();
+	imageImport->SetDataScalarTypeToUnsignedChar();
+	imageImport->SetNumberOfScalarComponents(1);
+	imageImport->SetImportVoidPointer(cImage); //cImage - wskaŸnik do macierzy
+	imageImport->Update();
+
+	vtkSmartPointer<vtkImageActor> actor =
+		vtkSmartPointer<vtkImageActor>::New();
+#if VTK_MAJOR_VERSION <= 5
+	actor->SetInput(imageImport->GetOutput());
+#else
+	actor->SetInputData(imageImport->GetOutput());
+#endif
+
+	vtkSmartPointer<vtkRenderer> renderer =
+		vtkSmartPointer<vtkRenderer>::New();
+	renderer->AddActor(actor);
+	renderer->ResetCamera();
+
+	vtkSmartPointer<vtkRenderWindow> renderWindow =
+		vtkSmartPointer<vtkRenderWindow>::New();
+	renderWindow->AddRenderer(renderer);
+
+	vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
+		vtkSmartPointer<vtkRenderWindowInteractor>::New();
+	vtkSmartPointer<vtkInteractorStyleImage> style =
+		vtkSmartPointer<vtkInteractorStyleImage>::New();
+
+	renderWindowInteractor->SetInteractorStyle(style);
+
+	renderWindowInteractor->SetRenderWindow(renderWindow);
+	renderWindowInteractor->Initialize();
+
+	renderWindowInteractor->Start();
+
+	//return EXIT_SUCCESS;
+}
+
+void Visualization2D::InitValue() {
+	ui->setupUi(this);
+}
+
+void Visualization2D::SetConnections() {
+	connect(ui->visualizeBtn, SIGNAL(clicked(bool)), this, SLOT(Brain2D()));
+}
+
+void Visualization2D::InitUI() {
+}
---
+++ x64/DADM/DADM/visualization2D.h
@@ -0,0 +1,60 @@
+#ifndef VISUALIZATION2D_H
+#define VISUALIZATION2D_H
+#include <vtkSmartPointer.h>
+#include <vtkObjectFactory.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+#include <vtkActor.h>
+#include <vtkImageViewer2.h>
+#include <vtkInteractorStyleImage.h>
+#include <vtkActor2D.h>
+#include <vtkTextProperty.h>
+#include <vtkTextMapper.h>
+#include <vtkTextRenderer.h>
+#include <sstream>
+#include <vtkStructuredPointsReader.h>
+#include <vtkPolyDataConnectivityFilter.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
+#include <vtkCamera.h>
+#include <vtkProperty.h>
+#include <vtkImageReader.h>
+#include <vtkNamedColors.h>
+#include <vtkGenericOpenGLRenderWindow.h>
+#include <vtkBorderWidget.h>
+#include <vtkPlane.h>
+#include <vtkImplicitPlaneRepresentation.h>
+#include <vtkClipPolyData.h>
+#include <vtkImplicitPlaneWidget2.h>
+#include <QVTKOpenGLWidget.h>
+#include <QMainWindow>
+#include <QThread>
+#include <QVTKOpenGLWidget.h>
+#include "Brain_2D.h"
+
+
+namespace Ui {
+	class Visualization2D;
+}
+
+class Visualization2D : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	explicit Visualization2D(QWidget *parent = 0);
+	 
+private:
+	Ui::Visualization2D *ui;
+	Brain_2D *brain_2D;
+
+	void InitValue();
+	void InitUI();
+	void SetConnections();
+
+	private slots:
+	void Brain2D();
+};
+
+#endif VISUALIZATION2D_H
