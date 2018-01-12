4c8b0b3ec6ccfb4776b9a970ded638d68a058460
Aneta <aneti9862@gmail.com>
2018-01-12 02:59:44 +0100

    Brain 3D  - wczytywanie pliku mat

--- x64/DADM/DADM/visualization3d.cpp
+++ x64/DADM/DADM/visualization3d.cpp
@@ -1,9 +1,11 @@
 #include "visualization3d.h"
+#include "Globals.h"
 
 #pragma region VISUALIZATION3D class
+
 Visualization3D::Visualization3D(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::Visualization3D)
+	QMainWindow(parent),
+	ui(new Ui::Visualization3D)
 {
 	InitValue();
 	SetConnections();
@@ -11,16 +13,23 @@ Visualization3D::Visualization3D(QWidget *parent) :
 }
 
 #pragma region controls event
-
 void Visualization3D::Brain3D() {
-	QUrl url = QFileDialog::getOpenFileUrl(this, "Open file", QUrl(""), "Raw file (*.raw) (*raw)");
+
+#pragma region to delete
+	// do usuniêcia, jeœli bêdzie gotowy ca³y strumieñ danych
+	// nale¿y wtedy odpowiedni¹ strukturê przypisaæ do zmiennej "inputData"
+	QUrl url = QFileDialog::getOpenFileUrl(this, "Open file", QUrl(""), "Mat file (*.mat) (*mat)");
 
 	if (url.isEmpty())
 		return;
 
-	QString path = url.path().remove(0, 1);
+	ReadMatFile(url.path());
+	inputData = Global::structuralData;
+#pragma endregion to delete
+
+	SetDataSize();
 	UpdateProcessStateText("The model 3D initializing ....\nPlease wait");
-	brain_3D = new Brain_3D(path, xspace, yspace, zspace, threshold, shrinkingFactor);
+	brain_3D = new Brain_3D(inputData, xspace, yspace, zspace, threshold, shrinkingFactor);
 
 	worker = new VisualizationWorker(brain_3D);
 	connect(worker, &VisualizationWorker::ModelCreationDone, this, &Visualization3D::AddRendererAndPlaneWidget);
@@ -29,19 +38,20 @@ void Visualization3D::Brain3D() {
 }
 
 void Visualization3D::SliderValueChanged(int sliderValue) {
-    ui -> thresholdTextEdit -> setText(QString::number(sliderValue));
+	ui->thresholdTextEdit->setText(QString::number(sliderValue));
 }
 
 void Visualization3D::ShrinkSliderValueChanged(int sliderValue) {
 	shrinkingFactor = sliderValue;
-	
+
 	if (visualizationDone) {
 		UpdateProcessStateText("The mri data shrinking...\nPlease wait");
 		QEventLoop loop;
 		QTimer::singleShot(1000, &loop, SLOT(quit()));
 		loop.exec();
-		brain_3D -> setShrinkFactor(shrinkingFactor);
-		ui -> qvtkWidget -> GetRenderWindow() -> Render();
+
+		brain_3D->setShrinkFactor(shrinkingFactor);
+		ui->qvtkWidget->GetRenderWindow()->Render();
 		UpdateProcessStateText("Shrinking done");
 	}
 }
@@ -53,11 +63,11 @@ void Visualization3D::CutEnableChanged(bool cutEnable) {
 			planeWidget->On();
 		}
 		else {
-			brain_3D->getMapper()->SetInputConnection(brain_3D->getMarchingCubes()->GetOutputPort());
+			brain_3D->getMapper()->SetInputConnection(brain_3D->getDecimate()->GetOutputPort());
 			planeWidget->Off();
 		}
 
-		ui -> qvtkWidget -> GetRenderWindow() -> Render();
+		ui->qvtkWidget->GetRenderWindow()->Render();
 	}
 	else {
 		if (cutEnable)
@@ -68,7 +78,7 @@ void Visualization3D::CutEnableChanged(bool cutEnable) {
 }
 
 void Visualization3D::AcceptThreshold() {
-	QString thresholdString = ui -> thresholdTextEdit -> toPlainText();
+	QString thresholdString = ui->thresholdTextEdit->toPlainText();
 	float thresholdFloat = thresholdString.toFloat();
 	threshold = thresholdFloat;
 
@@ -79,11 +89,11 @@ void Visualization3D::AcceptThreshold() {
 		QTimer::singleShot(1000, &loop, SLOT(quit()));
 		loop.exec();
 
-		MarchingCubes mc = brain_3D -> getMarchingCubes();
-		mc -> SetValue(0, thresholdFloat);
-		brain_3D -> setMarchingCubes(mc);
-		brain_3D -> setThreshold(threshold);
-		ui->qvtkWidget -> GetRenderWindow() -> Render();
+		MarchingCubes mc = brain_3D->getMarchingCubes();
+		mc->SetValue(0, thresholdFloat);
+		brain_3D->setMarchingCubes(mc);
+		brain_3D->setThreshold(threshold);
+		ui->qvtkWidget->GetRenderWindow()->Render();
 
 		UpdateProcessStateText("Rendering done");
 	}
@@ -96,13 +106,16 @@ void Visualization3D::AcceptThreshold() {
 #pragma region private methods
 void Visualization3D::InitValue() {
 	ui->setupUi(this);
-	xspace = 180;
-	yspace = 216;
-	zspace = 180;
-	threshold = 32;
+	threshold =3;
 	shrinkingFactor = 2;
 	visualizationDone = false;
 	setCutOptionEnable = true;
+}
+
+void Visualization3D::SetDataSize() {
+	xspace = inputData[0].rows();
+	yspace = inputData[0].cols();
+	zspace = inputData.size();
 }
 
 void Visualization3D::SetConnections() {
@@ -149,7 +162,7 @@ void Visualization3D::AddRendererAndPlaneWidget() {
 	if (setCutOptionEnable)
 		planeWidget->On();
 
-	clipper->SetInputConnection(brain_3D->getMarchingCubes()->GetOutputPort());
+	clipper->SetInputConnection(brain_3D->getDecimate()->GetOutputPort());
 	brain_3D->getMapper()->SetInputConnection(clipper->GetOutputPort());
 
 	plane->SetOrigin(xspace / 2., yspace / 2., zspace / 2.);
@@ -179,20 +192,60 @@ void Visualization3D::AddRendererAndPlaneWidget() {
 void Visualization3D::UpdateProcessStateText(QString text) {
 	ui->processDescLabel->setText(text);
 }
+
+void Visualization3D::ReadMatFile(QString path) {
+
+	QByteArray ba = path.remove(0, 1).toLatin1();
+	const char *fileName = ba.data();
+	mat_t *mat = Mat_Open(fileName, MAT_ACC_RDONLY);
+
+	if (mat) {
+		matvar_t *matVar = 0;
+		matVar = Mat_VarRead(mat, (char*)"imageMaskFull");
+
+		if (matVar) {
+			const double *xData = static_cast<const double*>(matVar->data);
+			std::vector<MatrixXd> data;
+			int val_num = 0;
+
+			qDebug() << matVar->dims[0];
+			qDebug() << matVar->dims[1];
+			qDebug() << matVar->dims[2];
+
+			for (int i = 0; i < matVar->dims[2]; i++) {
+				MatrixXd m(matVar->dims[0], matVar->dims[1]);
+				for (int j = 0; j < matVar->dims[1]; j++) {
+					for (int k = 0; k < matVar->dims[0]; k++) {
+						m(k, j) = xData[val_num];
+						val_num++;
+						if (val_num >= matVar->dims[0] * matVar->dims[1] * matVar->dims[2]) break;
+					}
+				}
+				data.push_back(m);
+			}
+
+			Global::structuralData = data;
+		}
+
+		Mat_Close(mat);
+	}
+}
 #pragma endregion private methods
 
 #pragma endregion VISUALIZATION3D class
 
-
 #pragma region WORKER class
+
 VisualizationWorker::VisualizationWorker(Brain_3D *brain_3D) : brain_3D(brain_3D) {
 	qRegisterMetaType<Renderer>("Renderer");
 	qRegisterMetaType<MarchingCubes>("MarchingCubes");
 	qRegisterMetaType<MarchingCubes>("Mapper");
+	qRegisterMetaType<MarchingCubes>("Decimate");
 }
 
 void VisualizationWorker::run() {
 	brain_3D->Start();
 	emit ModelCreationDone();
 }
+
 #pragma endregion WORKER class
