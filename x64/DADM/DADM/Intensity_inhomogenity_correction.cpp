b297b36aa5e986817c6b34a2edfaaf0e064f12ba
adagurgacz <adagurgacz@op.pl>
2018-01-16 21:04:16 +0100

    initial commit

--- x64/DADM/DADM/Intensity_inhomogenity_correction.cpp
+++ x64/DADM/DADM/Intensity_inhomogenity_correction.cpp
@@ -1,4 +1,5 @@
 #include "Intensity_inhomogenity_correction.h"
+#include "Globals.h"
 #include <iostream>
 #include <iomanip>
 #include <fstream>
@@ -6,7 +7,14 @@
 #include "qdebug.h"
 #include <Eigen/Eigen>
 #include <Eigen/Dense>
-//#include <unsupported/Eigen/NonLinearOptimization>
+#include <unsupported/Eigen/NonLinearOptimization>
+#include "matio.h"
+#include "qaction.h"
+#include "qfiledialog.h"
+#define _USE_MATH_DEFINES
+#include "qmath.h"
+#include <cmath>
+
 
 Intensity_inhomogenity_correction::Intensity_inhomogenity_correction(Data3D data)
 {
@@ -22,42 +30,173 @@ Intensity_inhomogenity_correction::Intensity_inhomogenity_correction(Data4D data
 	dtype = DIFFUSION_DATA;
 }
 
+void Intensity_inhomogenity_correction::SetDataSize() {
+	xspace = inputData[0].rows();
+	yspace = inputData[0].cols();
+	zspace = inputData.size();
+}
+Eigen::MatrixXd Intensity_inhomogenity_correction::selectRandomPoints(Eigen::MatrixXd &image, int n) {
+	
+	Eigen::MatrixXd values;
+	
+	for (int i = 0; i < n; i++) {
+		int coordx = ceil(rand() * 256);
+		int coordy = ceil(rand() * 256);
+		values = image(coordx, coordy);
+	}
+	return values;
+}
+void Intensity_inhomogenity_correction::createFilter()
+{
+	// set standard deviation to 1.0
+	double sigma = 1.0;
+	double r, s = 2.0 * sigma * sigma;
+	double gKernel[5][5];
+
+	// sum is for normalization
+	double sum = 0.0;
+	// generate 256x256 kernel
+	for (int x = -2; x <= 2; x++)
+	{
+		for (int y = -2; y <= 2; y++)
+		{
+			r = sqrt(x*x + y*y);
+			gKernel[x + 2][y + 2] = (exp(-(r*r) / s)) / (M_PI * s);
+			sum += gKernel[x + 2][y + 2];
+		}
+	}
+
+	// normalize the Kernel
+	for (int i = 0; i < 5; ++i)
+		for (int j = 0; j < 5; ++j)
+			gKernel[i][j] /= sum;
+}
+
+Eigen::MatrixXd Intensity_inhomogenity_correction::applyFilter(Eigen::MatrixXd image, double * gKernel) {
+	
+	//assert(image.size() == 3 && filter.size() != 0);
+
+	int height = image.rows();
+	int width = image.cols();
+	int filterHeight = sizeof(gKernel[0]);
+	int filterWidth = sizeof(gKernel);
+	int newImageHeight = height - filterHeight + 1;
+	int newImageWidth = width - filterWidth + 1;
+	int d, i, j, h, w;
+
+	Eigen::MatrixXd newImage;
+
+	
+		for (i = 0; i<newImageHeight; i++) {
+			for (j = 0; j<newImageWidth; j++) {
+				for (h = i; h<i + filterHeight; h++) {
+					for (w = j; w<j + filterWidth; w++) {
+						newImage(i,j) += gKernel[h - i][w - j] * image(h,w);
+					}
+				}
+			}
+		}
+	
+
+	return newImage;
+};
+
+void Intensity_inhomogenity_correction::importTest3D()
+{
+	
+	QString path = "C:/Users/Maurycy/Desktop/studia/projektDADM/DADM/DADM/x64/DADM/DADM";
+	QByteArray ba = path.toLatin1();
+	//const char *fileName = ba.data();
+	const char *fileName = "T1_synthetic_normal_1mm_L8_r2.mat";
+	mat_t *mat = Mat_Open(fileName, MAT_ACC_RDONLY);
+
+	if (mat) {
+		qDebug() << "Otwarto plik";
+
+		matvar_t *matVar = 0;
+
+		matVar = Mat_VarRead(mat, (char*)"In");
+
+		if (matVar) {
+			qDebug() << "Otwarto zmienn¹";
+
+			//unsigned int xSize = matVar->nbytes / matVar->data_size;
+			const double *xData = static_cast<const double*>(matVar->data);
+
+			qDebug() << matVar->dims[0];
+			qDebug() << matVar->dims[1];
+
+			//MatrixXcd m(matVar->dims[0], matVar->dims[1]);
+			int val_num = 0;
+			MatrixXd m(matVar->dims[0], matVar->dims[1]);
+			for (int i = 0; i < matVar->dims[1]; i++) {
+				for (int j = 0; j < matVar->dims[0]; j++) {
+					m(j, i) = xData[val_num];
+					val_num++;
+					if (val_num >= matVar->dims[0] * matVar->dims[1]) break;
+					qDebug() << xData[val_num];
+				}
+			}
+			image = m;
+		}
+	}
+	if (mat) {
+		qDebug() << "Otwarto plik";
+
+		matvar_t *matVar = 0;
+
+		matVar = Mat_VarRead(mat, (char*)"MapaR2");
+
+		if (matVar) {
+			qDebug() << "Otwarto zmienn¹";
+
+			//unsigned int xSize = matVar->nbytes / matVar->data_size;
+			const double *xData = static_cast<const double*>(matVar->data);
+
+			qDebug() << matVar->dims[0];
+			qDebug() << matVar->dims[1];
+
+			//MatrixXcd m(matVar->dims[0], matVar->dims[1]);
+			int val_num = 0;
+			MatrixXd m(matVar->dims[0], matVar->dims[1]);
+			for (int i = 0; i < matVar->dims[1]; i++) {
+				for (int j = 0; j < matVar->dims[0]; j++) {
+					m(j, i) = xData[val_num];
+					val_num++;
+					if (val_num >= matVar->dims[0] * matVar->dims[1]) break;
+					qDebug() << xData[val_num];
+				}
+			}
+			sigma = m.cwiseProduct(m);
+		}
+	}
+
+}
 struct LMFunctor
 {
-	// 'm' pairs of (x, f(x))
-	Eigen::MatrixXf measuredValues;
+	// image
+	Eigen::MatrixXd measuredValues;
 
-	// Compute 'm' errors, one for each data point, for the given parameter values in 'x'
-	int operator()(const Eigen::VectorXf &x, Eigen::VectorXf &fvec) const
+	int operator()(const Eigen::MatrixXd &x, Eigen::MatrixXd &fvec) const
 	{
-		// 'x' has dimensions n x 1
-		// It contains the current estimates for the parameters.
-
-		// 'fvec' has dimensions m x 1
-		// It will contain the error for each data point.
-
 		float aParam = x(0);
 		float bParam = x(1);
 		float cParam = x(2);
 
 		for (int i = 0; i < values(); i++) {
-			float xValue = measuredValues(i, 0);
-			float yValue = measuredValues(i, 1);
+			for (int j = 0; j < values(); j++) {
+				float xValue = x(i, j);
+				float yValue = measuredValues(i, j);
 
-			fvec(i) = yValue - (aParam * xValue * xValue + bParam * xValue + cParam);
+				fvec(i,j) = yValue - (aParam * xValue * xValue + bParam * xValue + cParam);
+			}
 		}
 		return 0;
 	}
 
 	// Compute the jacobian of the errors
-	int df(const Eigen::VectorXf &x, Eigen::MatrixXf &fjac) const
+	int df(const Eigen::MatrixXd &x, Eigen::MatrixXd &fjac) const
 	{
-		// 'x' has dimensions n x 1
-		// It contains the current estimates for the parameters.
-
-		// 'fjac' has dimensions m x n
-		// It will contain the jacobian of the errors, calculated numerically in this case.
-
 		float epsilon;
 		epsilon = 1e-5f;
 
@@ -67,13 +206,13 @@ struct LMFunctor
 			Eigen::VectorXf xMinus(x);
 			xMinus(i) -= epsilon;
 
-			Eigen::VectorXf fvecPlus(values());
+			Eigen::MatrixXd  fvecPlus(values());
 			operator()(xPlus, fvecPlus);
 
-			Eigen::VectorXf fvecMinus(values());
+			Eigen::MatrixXd  fvecMinus(values());
 			operator()(xMinus, fvecMinus);
 
-			Eigen::VectorXf fvecDiff(values());
+			Eigen::MatrixXd  fvecDiff(values());
 			fvecDiff = (fvecPlus - fvecMinus) / (2.0f * epsilon);
 
 			fjac.block(0, i, values(), 1) = fvecDiff;
@@ -96,51 +235,14 @@ struct LMFunctor
 
 };
 
+void Intensity_inhomogenity_correction::LMOptimization(){
 
-void Intensity_inhomogenity_correction::StructuralDataAlgorithm() {
-	// Goal
-	//
-	// Given a non-linear equation: f(x) = a(x^2) + b(x) + c
-	// and 'm' data points (x1, f(x1)), (x2, f(x2)), ..., (xm, f(xm))
-	// our goal is to estimate 'n' parameters (3 in this case: a, b, c)
-	// using LM optimization.
-	//
+	importTest3D();
+	Eigen::MatrixXd measuredValues = sigma;
+	//inputData = Global::structuralData;
+	int m = 256;
+	int k = 256;
 
-	//
-	// Read values from file.
-	// Each row has two numbers, for example: 5.50 223.70
-	// The first number is the input value (5.50) i.e. the value of 'x'.
-	// The second number is the observed output value (223.70),
-	// i.e. the measured value of 'f(x)'.
-	//
-
-	std::ifstream infile("measured_data.txt");
-	if (!infile) {
-		std::cout << "Unable to read file." << std::endl;
-	}
-
-	std::vector<float> x_values;
-	std::vector<float> y_values;
-
-	std::string line;
-	while (getline(infile, line)) {
-		std::istringstream ss(line);
-		float x, y;
-		ss >> x >> y;
-		x_values.push_back(x);
-		y_values.push_back(y);
-	}
-
-	// 'm' is the number of data points.
-	int m = x_values.size();
-
-	// Move the data into an Eigen Matrix.
-	// The first column has the input values, x. The second column is the f(x) values.
-	Eigen::MatrixXf measuredValues(m, 2);
-	for (int i = 0; i < m; i++) {
-		measuredValues(i, 0) = x_values[i];
-		measuredValues(i, 1) = y_values[i];
-	}
 
 	// 'n' is the number of parameters in the function.
 	// f(x) = a(x^2) + b(x) + c has 3 parameters: a, b, c
@@ -164,10 +266,10 @@ void Intensity_inhomogenity_correction::StructuralDataAlgorithm() {
 	functor.m = m;
 	functor.n = n;
 
-	//***************odkomentowaæ te 3 linie:
-	//Eigen::LevenbergMarquardt<LMFunctor, float> lm(functor);
-	//int status = lm.minimize(x);
-	//std::cout << "LM optimization status: " << status << std::endl;
+
+	Eigen::LevenbergMarquardt<LMFunctor, float> lm(functor);
+	int status = lm.minimize(x);
+	std::cout << "LM optimization status: " << status << std::endl;
 
 	//
 	// Results
@@ -177,6 +279,10 @@ void Intensity_inhomogenity_correction::StructuralDataAlgorithm() {
 	std::cout << "\ta: " << x(0) << std::endl;
 	std::cout << "\tb: " << x(1) << std::endl;
 	std::cout << "\tc: " << x(2) << std::endl;
+}
+
+void Intensity_inhomogenity_correction::StructuralDataAlgorithm() {
+	
 }
 
 void Intensity_inhomogenity_correction::DiffusionDataAlgorithm() {
--- x64/DADM/DADM/Intensity_inhomogenity_correction.h
+++ x64/DADM/DADM/Intensity_inhomogenity_correction.h
@@ -12,5 +12,14 @@ public:
 private:
 	virtual void StructuralDataAlgorithm();
 	virtual void DiffusionDataAlgorithm();
+	Data3D inputData;			//dane w formacie data3D
+	void SetDataSize();			//zwraca rozmiary danych
+	void importTest3D();		//wczytanie danych testowych z pliku .mat
+	Eigen::MatrixXd selectRandomPoints(Eigen::MatrixXd &image, int n);	//wybranie losowych punktów z obrazu
+	void createFilter();		//tworzy macierz 5x5 parametrówfiltru Gaussa
+	Eigen::MatrixXd applyFilter(Eigen::MatrixXd image, double * gKernel);	//wymna¿a filtr przez obraz
+	void LMOptimization();		//oblicza wspólczynniki dopasowanej powierzchni
+	int xspace, yspace, zspace;	//rozmiary danych
+	Eigen::MatrixXd image,sigma;//zmienne przechowuj¹ce obraz na poszczególnych etapach
 };
 
