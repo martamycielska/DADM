a5c1fced727518a36c48d679b191115bfeb37438
adagurgacz <adagurgacz@op.pl>
2018-01-16 22:15:58 +0100

    add function declaration

--- x64/DADM/DADM/Intensity_inhomogenity_correction.h
+++ x64/DADM/DADM/Intensity_inhomogenity_correction.h
@@ -15,6 +15,7 @@ private:
 	Data3D inputData;			//dane w formacie data3D
 	void SetDataSize();			//zwraca rozmiary danych
 	void importTest3D();		//wczytanie danych testowych z pliku .mat
+	void writeToCSVfile(std::string name, MatrixXd matrix);	//zapis danych do pliku csv
 	Eigen::MatrixXd selectRandomPoints(Eigen::MatrixXd &image, int n);	//wybranie losowych punktów z obrazu
 	void createFilter();		//tworzy macierz 5x5 parametrówfiltru Gaussa
 	Eigen::MatrixXd applyFilter(Eigen::MatrixXd image, double * gKernel);	//wymna¿a filtr przez obraz
